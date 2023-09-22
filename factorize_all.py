6c0d77af89aec2974d279dc25b16e0303530d260
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-22 23:36:51 +0100

    task 0

--- factorize_all.py
+++ factorize_all.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env /usr/bin/python3
 
 def read_and_print_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if line.isdigit():
-                    n = int(line)
-                    if int(line) <= 1:
-                        print(f"{n} is not factorizable (prime or 1)")
-                    else:
-                        factors = factorize_all(n)
-                        if factors:
-                            p, q = factors
-                            print("{}={}*{}".format(n, q, p))
-                        else:
-                            print(f"{n} is not factorizable")
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.isdigit():
+                n = int(line)
+                if int(line) <= 1:
+                    print(f"{n} is not factorizable (prime or 1)")
                 else:
-                    print(f"{line} is not a number")
-    except FileNotFoundError:
-        print(f"Error: File not found - {file_path}")
-    except Exception as e:
-        print(f"Error: {e}")
+                    factors = factorize_all(n)
+                    if factors:
+                        p, q = factors
+                        print("{}={}*{}".format(n, q, p))
+                    else:
+                        print(f"{n} is not factorizable")
+            else:
+                print(f"{line} is not a number")  
     return None
 
 
 def factorize_all(line):
-    try:
-        for i in range(2, line):
-            if line % i == 0:
-                return i, line // i 
-    except Exception as e:
-        print(f"Error: {e}")
+    for i in range(2, line):
+        if line % i == 0:
+            return i, line // i 
     return None
