2604f8fdd1340f9bd6be989f0f6c01d273498204
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-24 12:10:48 +0100

    task 0

--- factorize_all.py
+++ factorize_all.py
@@ -10,14 +10,13 @@ def read_and_print_file(file_path):
                     n = int(line)
                     if int(line) <= 1:
                         print(f"{n} is not factorizable (prime or 1)")
-                    elif not is_prime(n):
-                        factors = factorize_all(n)
-                    if factors:
-                            if len(factors) == 2:
-                                p, q = factors
-                                print("{}={}*{}".format(n, q, p))
                     else:
-                        print(f"{n} is not factorizable")
+                        factors = factorize_all(n)
+                        if factors:
+                            p, q = factors
+                            print("{}={}*{}".format(n, q, p))
+                        else:
+                            print(f"{n} is not factorizable")
                 else:
                     print(f"{line} is not a number")
     except FileNotFoundError:
@@ -34,36 +33,5 @@ def factorize_all(line):
         else:
             for i in range(3, line, 2):
                 if line % i == 0:
-                    factor = pollard_rho(i)
-                    return factor, line // factor
-
-def pollard_rho(n):
-    x = random.randint(1, n - 1)
-    y = x
-    c = random.randint(1, n - 1)
-    d = 1
-    while d == 1:
-        x = (x * x + c) % n
-        y = (y * y + c) % n
-        y = (y * y + c) % n
-        d = gcd(abs(x - y), n)
-    return d
-
-
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    if num % 2 == 0:
-        return False
-    for i in range(3, int(math.sqrt(num)) + 1, 2):
-        if num % i == 0:
-            return False
-    return True
+                    return i, line // i
+    return None, None
