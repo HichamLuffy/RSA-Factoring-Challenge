7df269b25254c969801dc93d9b9b7c1ef81388a4
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-27 21:51:48 +0100

    p 0.5

--- factorize_all.py
+++ factorize_all.py
@@ -3,12 +3,8 @@
 def factorize_all(line):
     if line <= 1:
         return None, None
-    if line % 2 == 0:
-        print("{}={}*{}".format(line, line // 2, 2))
-        return line // 2, 2
-    else:
-        for i in range(3, line // 2, 2):
-            if line % i == 0:
-                    print("{}={}*{}".format(line, line // i, i))
-                    return line // i, i
+    for i in range(2, int(line**0.5) + 1):
+        if line % i == 0:
+            print("{}={}*{}".format(line, line // i, i))
+            return
     return None, None
--- rsa.py
+++ rsa.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env /usr/bin/python3
 
-def read_and_print_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if line.isdigit():
-                    n = line
-                    if int(line) <= 1:
-                        print(f"{n} is not factorizable (prime or 1)")
-                    else:
-                        factors = factorize_all(n)
-                        if factors:
-                            p, q = factors
-                            print("{}={}*{}".format(n, q, p))
-                        else:
-                            print(f"{n} is not factorizable")
-                else:
-                    print(f"{line} is not a number")
-                break
-    except FileNotFoundError:
-        print(f"Error: File not found - {file_path}")
-    except Exception as e:
-        print(f"Error: {e}")
-    return None
+
 
 
 def factorize_all(line):
